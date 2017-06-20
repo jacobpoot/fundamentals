ab1048b6bbc4d485c036dc5656a774fb19054ab1
Jacob Poot <jacob.poot@ogd.nl>
2017-06-20 15:29:33 +0200

    added VM

--- main.tf
+++ main.tf
@@ -2,3 +2,39 @@ resource "azurerm_resource_group" "test" {
   name     = "acctestrg"
   location = "West US"
 }
+
+resource "azurerm_virtual_machine" "test" {
+  name                  = "acctvm"
+  location              = "West US"
+  resource_group_name   = "${azurerm_resource_group.test.name}"
+  network_interface_ids = ["${azurerm_network_interface.test.id}"]
+  vm_size               = "Standard_A0"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "14.04.2-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name          = "myosdisk1"
+    vhd_uri       = "${azurerm_storage_account.test.primary_blob_endpoint}${azurerm_storage_container.test.name}/myosdisk1.vhd"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags {
+    environment = "staging"
+  }
+}
